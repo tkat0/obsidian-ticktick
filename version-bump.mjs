915d02a07acf61d59de49f54aaf3245a766f39ae
tkat0 <tkato.dev@gmail.com>
2022-10-01 07:06:34 +0900

    chore: fix release process

--- version-bump.mjs
+++ version-bump.mjs
@@ -7,6 +7,7 @@ let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
+writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
